34f40883f4d4355678c1667371bb7614b18e3a94
Prajwal Vishal Jain <56448543+j836@users.noreply.github.com>
2021-03-24 05:38:49 -0700

    Removed add function

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,9 +2,5 @@ class HelloWorld {
   public static void main(String args[]) {
     System.out.println("Hello World");
     }
-}
 
-static void add(int num1,int num2) {
-  System.out.println(num1 + num2);
-}
 }
